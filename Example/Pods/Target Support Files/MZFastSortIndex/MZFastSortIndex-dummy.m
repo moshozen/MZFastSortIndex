19627ede4d758e3f0a43b465c52ebb3f3264236a
Mat Trudel <mat@geeky.net>
2016-03-14 11:06:02 -0400

    Initial commit `pod lib create MZFastSortIndex`

---
+++ Example/Pods/Target Support Files/MZFastSortIndex/MZFastSortIndex-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MZFastSortIndex : NSObject
+@end
+@implementation PodsDummy_MZFastSortIndex
+@end
---
+++ Example/Pods/Target Support Files/Pods-MZFastSortIndex_Tests/Pods-MZFastSortIndex_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MZFastSortIndex_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MZFastSortIndex_TestsVersionString[];
+
